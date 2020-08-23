568e2517d6f860afcc545d7ff5894374950e474c
Kishore Newton <kishorenke@gmail.com>
2020-08-23 20:37:40 +0530

    added middleware

--- app.js
+++ app.js
@@ -4,6 +4,12 @@ const exphbs = require('express-handlebars')
 const nodemailer = require('nodemailer')
 const app = express()
 
+app.engine('handlebars', exphbs())
+app.set('view engine', 'handlebars')
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
 app.get('/', (req, res) => {
     res.send('hello')
 })
